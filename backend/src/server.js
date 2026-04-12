36ee0efcd2a840198ceb1e3ec8560d28ab7d3c1f
Hiba <khiba9543@gmail.com>
2026-04-13 00:44:39 +0530

    Some changes made

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
 
 const app = express();
 
@@ -12,25 +13,34 @@ const PORT = process.env.PORT || 5001;
 
 
 // Middleware
-app.use(cors({
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({
     origin: "http://localhost:5173",
-}));
+  }));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 
-// app.use((req, res, next) => {
-//     console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`);
-//     next();
-// });
-
 // Routes
 app.use("/api/notes", notesRoutes);
 
-// Connect DB
-connectDB().then(() => {
 
-    app.listen(PORT, () => {
-        console.log("Server started on PORT:", PORT);
-    });
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../../frontend/dist");
+
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
+
+// Start server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { Import } from "lucide-react";
 
+const BASE_URL = Import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BASE_URL,
 })
 
 export default api;
