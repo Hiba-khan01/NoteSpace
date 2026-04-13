1fd9f391d3c484fd13213dfd3c860a35b9bc1a2b
Hiba <khiba9543@gmail.com>
2026-04-13 22:10:23 +0530

    Final Deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,15 +27,13 @@ app.use("/api", rateLimiter);
 app.use("/api/notes", notesRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../../frontend/dist");
+const frontendPath = path.join(__dirname, "../../frontend/dist");
 
-  app.use(express.static(frontendPath));
+app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 
 // Start server
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,12 @@
 import axios from "axios";
-import { Import } from "lucide-react";
 
-const BASE_URL = Import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "/api";
+
 const api = axios.create({
-    baseURL: BASE_URL,
-})
+  baseURL: BASE_URL,
+});
 
 export default api;
